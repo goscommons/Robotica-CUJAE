c15234c0c7700a81ef5359f200418ede8693ea33
Carlos Ernesto Perez Capote <cperezcapote@gmail.com>
2019-02-07 01:22:47 -0500

    using custom theme vuepress-theme-gocommons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: 'Groups Portal Template',
   dest: 'public',
-  base: '/go-commons-website-template/'
+  base: '/go-commons-website-template/',
+  theme: 'vuepress-theme-gocommons',
 };
