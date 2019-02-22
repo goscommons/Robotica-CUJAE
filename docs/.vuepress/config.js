9e2297348082d6d3d957532977a2d1d30ba7b3f8
Jose Carlos Urra Llanusa <jose.urra86@gmail.com>
2019-02-22 13:55:37 +0100

    Navbar working, sidebar working only with one readme, the multiple sidebar is not working yet

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,17 +6,15 @@ module.exports = {
 
   themeConfig: {
     nav: [
-      { text: 'Intro', link: '/1_Intro/' },
+      { text: 'Intro', link: '/' },
       { text: 'Build', link: '/2_Build/' },
       { text: 'Contribute', link: '/3_Contribute/' },
       { text: 'Training', link: '/4_Training/' },
       { text: 'Kit', link: '/5_Kit/' },
     ],
 
-    sidebar: [
-      ['/the-project/', 'The project'],
-      ['/CODEofCONDUCT.md', 'Code of conduct'],
-    ]
+    displayAllHeaders: true, // Default: false
+    sidebar: 'auto'
 
   }
 };
