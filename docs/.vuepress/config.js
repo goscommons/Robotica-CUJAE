4ba512e7c988a07370c9312b96c4153d5e08c719
Jose Carlos Urra Llanusa <jose.urra86@gmail.com>
2019-02-22 15:00:26 +0100

    working different sidebars

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,15 +6,36 @@ module.exports = {
 
   themeConfig: {
     nav: [
-      { text: 'Intro', link: '/' },
+      { text: 'Intro', link: '/1_Intro/' },
       { text: 'Build', link: '/2_Build/' },
       { text: 'Contribute', link: '/3_Contribute/' },
       { text: 'Training', link: '/4_Training/' },
       { text: 'Kit', link: '/5_Kit/' },
     ],
 
-    displayAllHeaders: true, // Default: false
-    sidebar: 'auto'
+    sidebar: {
+      '/1_Intro/': [
+        '',
+        'Model',  /* /foo/one.html */
+        'Self-Assesment'   /* /foo/two.html */
+      ],
+
+      '/2_Build/': [
+        '',      /* /bar/ */
+        'Assembly', /* /bar/three.html */
+        'BOM'   /* /bar/four.html */
+      ],
+
+      '/3_Contribute/': [
+        '',      /* /bar/ */
+        'For-devs'
+      ],
+
+      // fallback
+      '/': [
+        ''
+      ]
+    }
 
   }
 };
